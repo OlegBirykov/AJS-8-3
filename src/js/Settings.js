ce809e2a4b2015641f1f0bba0663f7caf71b8673
OlegBirykov <p_petro75@mail.ru>
2020-07-25 21:39:27 +0300

    Добавлен код и тесты

--- src/js/Settings.js
+++ src/js/Settings.js
@@ -1,3 +1,22 @@
 export default class Settings {
+  constructor() {
+    this.defaultSettings = new Map([
+      ['theme', 'dark'],
+      ['music', 'trance'],
+      ['difficulty', 'easy'],
+    ]);
+    this.userSettings = new Map();
+  }
 
+  get settings() {
+    const keys = [...this.defaultSettings.keys()];
+    const result = new Map();
+    keys.forEach((key) => {
+      result.set(
+        key,
+        this.userSettings.has(key) ? this.userSettings.get(key) : this.defaultSettings.get(key),
+      );
+    });
+    return result;
+  }
 }
--- src/js/__tests__/Settings.test.js
+++ src/js/__tests__/Settings.test.js
@@ -1,5 +1,12 @@
 import Settings from '../Settings';
 
-test('', () => {
-  expect(new Settings()).toDefined();
+test('Должны изменяться только настройки, заданные пользователем', () => {
+  const settings = new Settings();
+  settings.userSettings.set('music', 'rock');
+  const result = new Map([
+    ['theme', 'dark'],
+    ['music', 'rock'],
+    ['difficulty', 'easy'],
+  ]);
+  expect(settings.settings).toEqual(result);
 });
